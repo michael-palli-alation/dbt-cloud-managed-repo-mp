5a0dc4038e24e9dec46be2fd6487a3ece2a41036
Michael Palli <michael.palli@alation.com>
2026-01-07 06:42:23 +0000

    testing changes to superstore

--- models/superstore.sql
+++ models/superstore.sql
@@ -1,14 +1,10 @@
 with
-    superstore ss as (
-        SELECT   ss.country,
-         ss.region,
-         ss.category,
-         ss.segment,
-         ss.sum(profit),
-         od.order_date,
-         od.customer_name
-FROM     superstore.public.superstore_reporting
-JOIN superstore.public.orders od on od.order_id = ss.order_id
+    superstore as (
+        select 'schema' as otype, schema_id as oid
+        from public.rdbms_schemas
+        union all
+        select 'table' as otype, table_id as oid
+        from public.rdbms_tables
     )
 select *
 from superstore

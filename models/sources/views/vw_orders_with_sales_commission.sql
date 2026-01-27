4210b9a72fdfa3396b92d3c456fd92b25014cc73
Michael Palli <michael.palli@alation.com>
2026-01-27 21:26:50 +0000

    changed to sales_target

--- models/sources/views/vw_orders_with_sales_commission.sql
+++ models/sources/views/vw_orders_with_sales_commission.sql
@@ -2,11 +2,11 @@ with o as (
     select * from {{ ref('stg_raw_orders') }}
 ),
 sal as (
-    select * from {{ ref('stg_analytics_sales_commission') }}
+    select * from {{ ref('stg_raw_sales_targets') }}
 )
 select 
  o.*,
  sal.*
 from orders o
-left join sales_commission sal
+left join sales_targets sal
   on o.order_date = sal.order_date

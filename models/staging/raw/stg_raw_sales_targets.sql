93c1bf743d5c9897500177675d74d23cea1533ec
Michael Palli <michael.palli@alation.com>
2026-01-27 11:46:50 +0000

    changed object type from varchar to string

--- models/staging/raw/stg_raw_sales_targets.sql
+++ models/staging/raw/stg_raw_sales_targets.sql
@@ -1,6 +1,6 @@
 select
-  category::varchar(100) as category,
+  category::string as category,
   order_date::date as order_date,
-  segment::varchar(50) as segment,
+  segment::string as segment,
   sales_target::number(38,0) as sales_target 
 from {{ source('superstore_public_raw', 'returns') }}
